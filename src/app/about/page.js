c80779c1338f8f10c10c1d78e659c7affaba967e
Amol Jangid <amoljangid33@gmail.com>
2023-09-06 13:31:48 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './about.css';
+const About = () => {
+  return (
+    <div>
+      <h1 className='about'>This is the About Page</h1>
+    </div>
+  )
+}
+
+export default About
